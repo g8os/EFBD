e917d56e4b438e2f5e3c61025fb27265db745d46
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-03-07 17:47:40 -0500

    add volumecontroller html docs to go:generate

--- nbdserver/generate.go
+++ nbdserver/generate.go
@@ -6,6 +6,9 @@ package main
 // VolumeController stub
 //go:generate go-raml server --ramlfile ../volumecontroller/volumeController.raml --dir stubs/volumecontroller --package volumecontroller --import-path "github.com/g8os/blockstor/nbdserver/stubs/volumecontroller"
 
+// VolumeController HTML docs
+//go:generate raml2html --input ../volumecontroller/volumeController.raml --output ../volumecontroller/volumeController.html
+
 // Storage backend clients
 //go:generate go-raml client --ramlfile clients/storagebackendcontroller/storagebackend.raml --dir clients/storagebackendcontroller/ --package storagebackendcontroller
 
