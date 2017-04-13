4d9322b7ae289f4bfc0df4adf80699a177cf301f
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-04-13 17:42:13 +0700

    tlog : travis : install c lib

---
+++ tlogserver/install_dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BUILD_DIR=/tmp/build_tlog
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+
+
+sudo apt-get install build-essential  autoconf automake
+# install isa-l
+cd $BUILD_DIR
+git clone https://github.com/01org/isa-l.git
+cd isa-l
+./autogen.sh
+./configure
+make
+sudo make install
+
+# install isa-l_crypto
+cd $BUILD_DIR
+git clone https://github.com/01org/isa-l_crypto.git
+cd isa-l_crypto
+./autogen.sh
+./configure
+make
+sudo make install
