1e1ad2f23e50e002b17de96aee913aa3ad981caa
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-04-13 17:58:59 +0700

    tlog : add yasm as dependencies.

--- tlogserver/install_dependencies.sh
+++ tlogserver/install_dependencies.sh
@@ -5,7 +5,7 @@ rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 
 
-sudo apt-get install build-essential  autoconf automake nasm
+sudo apt-get install build-essential  autoconf automake nasm yasm
 # install isa-l
 cd $BUILD_DIR
 git clone https://github.com/01org/isa-l.git
