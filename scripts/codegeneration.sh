ecb4e24654aec5cd3b991594a27ef38749c606f2
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-04-20 08:37:36 -0500

    update codegeneration fail message

--- scripts/codegeneration.sh
+++ scripts/codegeneration.sh
@@ -9,6 +9,6 @@ if [ -z "$GITSTATUS" ]; then
 	exit 0
 fi
 
-echo -e "changes detected, run 'go generate ./...' and commit generated code in these files:\n"
+echo -e "changes detected, run 'go generate ./gridapi' and commit generated code in these files:\n"
 echo "$GITSTATUS"
 exit 1
