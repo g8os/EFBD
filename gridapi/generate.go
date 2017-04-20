49a6ad7cab4da51a8d4cb8a131142306670f012d
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-04-20 08:16:29 -0500

    move grid (gen) ram files into its own dir

--- gridapi/generate.go
+++ gridapi/generate.go
@@ -1,10 +1,7 @@
 package gridapi
 
-//go:generate curl -O https://raw.githubusercontent.com/g8os/grid/1.1.0-alpha/raml/types.raml
-//go:generate curl -O https://raw.githubusercontent.com/g8os/grid/1.1.0-alpha/raml/api.raml
-
 // grid api client
-//go:generate go-raml client --ramlfile api.raml --dir gridapiclient --package gridapiclient
+//go:generate go-raml client --ramlfile gridspec/api.raml --dir gridapiclient --package gridapiclient
 
 // grid api stub
-//go:generate go-raml server --ramlfile api.raml --dir gridapistub --package gridapistub --no-apidocs
+//go:generate go-raml server --ramlfile gridspec/api.raml --dir gridapistub --package gridapistub --no-apidocs
