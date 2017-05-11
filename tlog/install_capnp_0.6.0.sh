6ae21e29a3b67e3a3bf9805edf4a4271293bca86
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-05-11 12:19:30 -0500

    travis: install capnp from source
    + Also document that we're using capnp version 0.6.0, so that developers and CI is in sync;
    + I would have preferred to use the debian package manager, but the capnp debian package seems to be out of date;

---
+++ tlog/install_capnp_0.6.0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ORIGDIR="$PWD"
+curl -O https://capnproto.org/capnproto-c++-0.6.0.tar.gz
+tar zxf capnproto-c++-0.6.0.tar.gz && rm -f capnproto-c++-0.6.0.tar.gz
+cd capnproto-c++-0.6.0 || (echo "couldn't download capnproto-c++-0.6.0" && exit 1)
+./configure
+sudo make install
+cd "$ORIGDIR" && sudo rm -rf capnproto-c++-0.6.0
