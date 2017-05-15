772d45423e3a0757d4343fbdf12c9d826fc9d39c
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-05-15 17:12:19 +0700

    tlog client : recreate tcp connection if broken.

--- tlog/tlogclient/client.go
+++ tlog/tlogclient/client.go
@@ -3,6 +3,7 @@ package tlogclient
 import (
 	"bufio"
 	"net"
+	"time"
 
 	"github.com/g8os/blockstor"
 )
@@ -31,17 +32,16 @@ type Client struct {
 }
 
 // New creates a new tlog client
-func New(addr string, vdiskID string) (*Client, error) {
-	conn, err := createConn(addr)
-	if err != nil {
-		return nil, err
-	}
-	return &Client{
+func New(addr, vdiskID string) (*Client, error) {
+	c := &Client{
 		addr:    addr,
 		vdiskID: vdiskID,
-		conn:    conn,
-		bw:      bufio.NewWriter(conn),
-	}, nil
+	}
+	if err := c.createConn(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
 }
 
 // Recv get channel of responses and errors (Result)
@@ -81,33 +81,70 @@ func (c *Client) RecvOne() (*Response, error) {
 	}, nil
 }
 
-func createConn(addr string) (*net.TCPConn, error) {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+func (c *Client) createConn() error {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", c.addr)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	conn.SetKeepAlive(true)
-	return conn, nil
+	c.conn = conn
+	c.bw = bufio.NewWriter(conn)
+	return nil
 }
 
 // Send sends the transaction tlog to server.
 // It returns error in these cases:
-// - broken network
-// - failed to send all tlog
-// in case of errors, client is not in valid state,
-// shouldn't be used anymore
-func (c *Client) Send(op uint8, seq uint64, lba, timestamp uint64,
+// - failed to encode the capnp.
+// - failed to recover from broken network connection.
+func (c *Client) Send(op uint8, seq, lba, timestamp uint64,
 	data []byte, size uint64) error {
 
+	var err error
+
 	hash := blockstor.HashBytes(data)
-	if err := c.encodeCapnp(op, seq, hash[:], lba, timestamp, data, size); err != nil {
-		return err
+
+	send := func() error {
+		if err = c.encodeCapnp(op, seq, hash[:], lba, timestamp, data, size); err != nil {
+			return err
+		}
+		return c.bw.Flush()
 	}
-	return c.bw.Flush()
+
+	okToSend := true
+
+	for i := 0; i < sendRetryNum+1; i++ {
+		if okToSend {
+			if err = send(); err == nil {
+				return nil
+			}
+
+			if _, ok := err.(net.Error); !ok {
+				return err // no need to rety if it is not network error.
+			}
+		}
+
+		// First sleep = 0 second,
+		// so we don't need to sleep in case of simple closed connection.
+		// We sleep in next iteration because there might be something error in
+		// the network connection or the tlog server that need time to be recovered.
+		time.Sleep(time.Duration(i) * time.Duration(sendSleepMs) * time.Millisecond)
+
+		if err = c.createConn(); err != nil {
+			okToSend = false
+		} else {
+			okToSend = true
+		}
+	}
+	return err
 }
+
+const (
+	sendRetryNum = 3
+	sendSleepMs  = 500
+)
