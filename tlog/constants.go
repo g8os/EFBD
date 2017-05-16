fbf9fb27d6b4b1c649c14d605d84b42232f3c5bf
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-05-15 22:24:38 -0500

    use NilHash as first aggr PrevHash value
    fixes #104

--- tlog/constants.go
+++ tlog/constants.go
@@ -1,5 +1,12 @@
 package tlog
 
+import "github.com/g8os/blockstor"
+
+var (
+	// used as the prevHash value for the first aggregation
+	FirstAggregateHash = blockstor.NilHash
+)
+
 const (
 	// last hash ardb key prefix
 	LastHashPrefix = "last_hash_"
