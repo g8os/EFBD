0e2502ceb3eafe8e6e64e33bc2ae053c10f296d4
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-07-21 00:35:57 -0500

    vendor+install missing capnpc-go plugin
    fixes #343

--- scripts/updatedependencies.py
+++ scripts/updatedependencies.py
@@ -2,9 +2,19 @@
 
 import os
 import json
+import errno
 import shutil
 
 from subprocess import call
+
+# util function to copy anything
+def copy_anything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc: # python >2.5
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else: raise
 
 # define the absolute location of the Godeps dependency file
 GO_SRC_DIR = os.path.join(os.environ["GOPATH"], "src")
@@ -31,6 +41,7 @@ call(["godep", "save", "-v", "./..."])
 # yes, it's a hack, just like this entire file is one
 NON_GO_DEPS = [
     "zombiezen.com/go/capnproto2/std/go.capnp",
+    "zombiezen.com/go/capnproto2/capnpc-go"
 ]
 VENDOR_DIR = os.path.join(ROOT_DIR, "vendor")
 
@@ -38,6 +49,6 @@ for rel_dep in NON_GO_DEPS:
     print "manually copying {}...".format(rel_dep)
     src = os.path.join(GO_SRC_DIR, rel_dep)
     dst = os.path.join(VENDOR_DIR, rel_dep)
-    shutil.copy2(src, dst)
+    copy_anything(src, dst)
 
 print "Done!"
