b51e3928785e5de1cbedb191625e605f61b17f14
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2017-09-06 12:59:29 -0500

    complete vdisk statistics logger with comments [WIP]
    + Still need to answer one open question;
    + Still need to add unit tests;

--- nbd/nbdserver/statistics/statistics.go
+++ nbd/nbdserver/statistics/statistics.go
@@ -2,17 +2,40 @@ package statistics
 
 import (
 	"context"
+	"math/big"
 	"time"
 
 	"github.com/zero-os/0-Disk/config"
 	"github.com/zero-os/0-Disk/log"
 )
 
+// ====
+// [TODO] Answer following questions:
+//
+//   Are we OK that due to irregular activity that it won't be uncommon
+// to have lower average aggregation statistics (iops/throughput) values.
+// Meaning that we might broadcast a low IOPS/throughput value,
+// not because the vdisk is performing bad, but because we have an interval period,
+// where we have one operation at the start, and another one closer to the end of the interval,
+// with a big gap in between.
+//    => I'm not sure if there is a good solution as long as we wish to keep average values,
+//       however this problem could be partly avoided by logging every second,
+//       as we'll have much more accurate values due to the fact that the gaps
+//       will be very tiny if they exist at all, hence not invalidating the data.
+//
+// ====
+
 // VdiskLogger defines an nbd  statistics logger interface
 type VdiskLogger interface {
+	// LogReadOperation logs a read operation,
+	// using it to keep track of the read IOPS and read throughput (in KiB/s).
 	LogReadOperation(bytes int64)
+	// LogWriteOperation logs a write operation,
+	// using it to keep track of the write IOPS and write throughput (in KiB/s).
 	LogWriteOperation(bytes int64)
 
+	// Close all open resources and
+	// stop all background goroutines linked to this vdiskLogger.
 	Close() error
 }
 
@@ -28,35 +51,77 @@ func NewVdiskLogger(ctx context.Context, configSource config.Source, vdiskID str
 	}
 
 	logger := &vdiskLogger{
-		ctx:                ctx,
+		// context-related values
+		ctx:        ctx,
+		cancelFunc: cancel,
+
+		// pre-computed statistics keys
 		readThroughputKey:  "vdisk.throughput.read@virt." + vdiskID,
 		readIOPSKey:        "vdisk.iops.read@virt." + vdiskID,
 		writeThroughputKey: "vdisk.throughput.write@virt." + vdiskID,
 		writeIOPSKey:       "vdisk.iops.write@virt." + vdiskID,
-		tags:               log.MetricTags{},
-		readDataCh:         make(chan int64, 8),
-		writeDataCh:        make(chan int64, 8),
-		configCh:           configCh,
-		cancelFunc:         cancel,
-		broadcastFunc:      broadcastStatistic,
+
+		// empty tags for now
+		tags: log.MetricTags{},
+		// configCh to keep track of incoming config changes,
+		// and used as the input for the metric tags of this logger
+		configCh: configCh,
+
+		// incoming bytes (data) channel
+		readDataCh:  make(chan int64, 8),
+		writeDataCh: make(chan int64, 8),
+
+		// delegator to do the actual broadcasting work
+		// for each (early) finished aggregation interval
+		broadcastFunc: broadcastStatistic,
 	}
 	go logger.background()
 	return logger, nil
 }
 
+// vdiskLogger is used to gather r/w iops/throughput statistics for a given vdisk.
 type vdiskLogger struct {
-	ctx                              context.Context
+	// the context used for all background purposes of this vdiskLogger
+	ctx context.Context
+	// cancel the ctx property,
+	// stopping the background thread of this logger,
+	// as well as the running vdisk's cluster's config watcher
+	cancelFunc context.CancelFunc
+
+	// precomputed keys for this vdisk,
+	// used to broadcast the statistics linked to these keys
 	readThroughputKey, readIOPSKey   string
 	writeThroughputKey, writeIOPSKey string
-	tags                             log.MetricTags
-	readDataCh, writeDataCh          chan int64
-	configCh                         <-chan config.VdiskNBDConfig
-	cancelFunc                       context.CancelFunc
-	broadcastFunc                    broadcastFunc
+
+	// the tags contain the clusterID information
+	tags log.MetricTags
+	// configCh used to ensure this logger is using
+	// at all times the most up to date config for its metric tags.
+	// this is important as switching to a different cluster,
+	// might explain a sudden change in the incoming stats values
+	configCh <-chan config.VdiskNBDConfig
+
+	// buffered bytes channels for read and write purposes,
+	// buffered to ensure that a vdisk is never blocked on collecting
+	// aggregated stats input
+	readDataCh, writeDataCh chan int64
+
+	// for all purposes other than testing,
+	// this func is equal to `broadcastStatistic`
+	broadcastFunc broadcastFunc
+
+	// aggregators used for write and read operations
+	writeAggregator, readAggregator vdiskAggregator
 }
 
+// broadcastFunc is an internal function definition,
+// such that we can overwrite the broadcastFunc logic for a given vdiskLogger,
+// for testing purposes.
 type broadcastFunc func(key string, value float64, tags log.MetricTags)
 
+// broadcastStatistics is the actual broadcast function used for production,
+// using the zero-os/0-log lib wrapped in our log module,
+// in order to do zero-os stats logging (level 10) over STDERR.
 func broadcastStatistic(key string, value float64, tags log.MetricTags) {
 	log.BroadcastStatistics(key, value, log.AggregationAverages, tags)
 }
@@ -77,61 +142,94 @@ func (vl *vdiskLogger) Close() error {
 	return nil
 }
 
+// the background worker for a vdisk logger,
+// keeping track of the vdisk's cluster config, buffering of incoming r/w bytes,
+// and broadcasting values at planned and early intervals.
 func (vl *vdiskLogger) background() {
 	var cfg config.VdiskNBDConfig
 	var bytes int64
 
-	var writeAggregator, readAggregator vdiskAggregator
-
 	for {
 		select {
+		// context is finished, log for one last time and exit
 		case <-vl.ctx.Done():
+			log.Debug("logging last minute vdisks statistics")
+			vl.broadcastReadStatistics()
+			vl.broadcastWriteStatistics()
+
 			log.Debug("exit vdiskLogger because context is done")
 			return
 
+		// one of our stats timers is finished
+		case <-vl.readAggregator.C:
+			vl.broadcastReadStatistics()
+		case <-vl.writeAggregator.C:
+			vl.broadcastWriteStatistics()
+
+		// config has updated, check if our tags change
 		case cfg = <-vl.configCh:
 			if vl.tags[clusterIDKey] != cfg.StorageClusterID {
 				vl.tags[clusterIDKey] = cfg.StorageClusterID
 
-				readAggregator.Stop()
-				writeAggregator.Stop()
+				vl.readAggregator.Stop()
+				vl.writeAggregator.Stop()
 			}
 
+		// incoming data
 		case bytes = <-vl.readDataCh:
-			readAggregator.TrackBytes(bytes)
-
+			vl.readAggregator.TrackBytes(bytes)
 		case bytes = <-vl.writeDataCh:
-			writeAggregator.TrackBytes(bytes)
-
-		// TODO: perhaps ensure that we also collect the last time we write,
-		//       so that we only average values where we were actually active
-
-		case st := <-readAggregator.C:
-			iops, throughput, duration := readAggregator.Reset(st)
-			vl.broadcastFunc(vl.readIOPSKey, float64(iops), vl.tags)
-			tv := float64(throughput) / float64(duration.Seconds())
-			vl.broadcastFunc(vl.readThroughputKey, tv, vl.tags)
-
-		case st := <-writeAggregator.C:
-			iops, throughput, duration := writeAggregator.Reset(st)
-			vl.broadcastFunc(vl.writeIOPSKey, float64(iops), vl.tags)
-			tv := float64(throughput) / float64(duration.Seconds())
-			vl.broadcastFunc(vl.writeThroughputKey, tv, vl.tags)
+			vl.writeAggregator.TrackBytes(bytes)
 		}
 	}
 }
 
-type vdiskAggregator struct {
-	C <-chan time.Time
+// internal func to reset the read aggregator
+// and broadcast its aggregated iops/throughput values.
+func (vl *vdiskLogger) broadcastReadStatistics() {
+	iops, throughput := vl.readAggregator.Reset()
+	if iops == 0 {
+		return
+	}
 
-	start            time.Time
-	timer            *time.Timer
-	iops, throughput int64
+	vl.broadcastFunc(vl.readIOPSKey, iops, vl.tags)
+	vl.broadcastFunc(vl.readThroughputKey, throughput, vl.tags)
 }
 
+// internal func to reset the write aggregator
+// and broadcast its aggregated iops/throughput values.
+func (vl *vdiskLogger) broadcastWriteStatistics() {
+	iops, throughput := vl.writeAggregator.Reset()
+	if iops == 0 {
+		return
+	}
+
+	vl.broadcastFunc(vl.writeIOPSKey, iops, vl.tags)
+	vl.broadcastFunc(vl.writeThroughputKey, throughput, vl.tags)
+}
+
+// vdiskAggregator is used to aggregate values for a given r/w direction.
+type vdiskAggregator struct {
+	// public timer channel,
+	// only ever set while this aggregator is active
+	C <-chan time.Time
+
+	// time values
+	start, end time.Time
+	timer      *time.Timer
+
+	// total values
+	iops       int64
+	throughput big.Float
+}
+
+// Track the operation and the bytes that go with it.
+// It also starts an interval (of an active period) in case one wasn't started yet.
+// Each tracking also moves the end time up, as it marks the positive limit of this interval.
 func (agg *vdiskAggregator) TrackBytes(bytes int64) {
 	agg.iops++
-	agg.throughput += bytes
+	agg.throughput.Add(&agg.throughput, big.NewFloat(0).SetInt64(bytes))
+	agg.end = time.Now()
 
 	if agg.timer == nil {
 		agg.start = time.Now()
@@ -140,6 +238,9 @@ func (agg *vdiskAggregator) TrackBytes(bytes int64) {
 	}
 }
 
+// Stop this vdisk aggregator,
+// this will result in the Reset function being called a bit later.
+// A manual stop can be useful in case we need to stop before the planned interval duration.
 func (agg *vdiskAggregator) Stop() {
 	if agg.timer == nil {
 		return
@@ -148,22 +249,44 @@ func (agg *vdiskAggregator) Stop() {
 	agg.timer.Stop()
 }
 
-func (agg *vdiskAggregator) Reset(st time.Time) (iops, throughput int64, duration time.Duration) {
-	iops = agg.iops
-	throughput = agg.throughput
-
-	duration = st.Sub(agg.start)
-	if duration > vdiskAggregationDuration {
-		duration = vdiskAggregationDuration
+// Reset returns the aggregate values as an average over the active duration,
+// and reset the aggregator's internal values.
+func (agg *vdiskAggregator) Reset() (iops, throughput float64) {
+	if agg.timer == nil {
+		return
 	}
 
+	// compute interval durations
+	dur := agg.end.Sub(agg.start)
+	dursecs := big.NewFloat(dur.Seconds())
+
+	// compute IOPS
+	bigIOPS := big.NewFloat(0).SetInt64(agg.iops)
+	iops, _ = bigIOPS.Quo(bigIOPS, dursecs).Float64()
+
+	// compute throughput
+	agg.throughput.Quo(&agg.throughput, vdiskThroughputScalar)
+	agg.throughput.Quo(&agg.throughput, dursecs)
+	throughput, _ = agg.throughput.Float64()
+
+	// reset all values
 	agg.timer, agg.C = nil, nil
-	agg.iops, agg.throughput = 0, 0
+	agg.iops = 0
+	agg.throughput.SetFloat64(0)
+
+	// return compute results
 	return
 }
 
+// MaxVdiskAggregationDuration defines the maximum aggregation duration
+// used for vdisk operation statistics.
+const MaxVdiskAggregationDuration = time.Second * 30
+
 const (
 	clusterIDKey = "clusterID"
-	// TODO: change value
-	vdiskAggregationDuration = time.Second * 5
+)
+
+var (
+	vdiskAggregationDuration = MaxVdiskAggregationDuration
+	vdiskThroughputScalar    = big.NewFloat(1024)
 )
