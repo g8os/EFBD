21114ab833a6645d8d9ecf93d48d66a2575e04fe
Iwan Budi Kusnanto <ibk@labhijau.net>
2017-09-12 16:59:36 +0700

    tlog generator : fetch blocks concurrently

--- tlog/generator/generator.go
+++ tlog/generator/generator.go
@@ -1,7 +1,10 @@
 package generator
 
 import (
+	"context"
 	"fmt"
+	"runtime"
+	"sync"
 
 	"gopkg.in/validator.v2"
 
@@ -80,20 +83,98 @@ func (g *Generator) GenerateFromStorage() error {
 	}
 	defer sourceStorage.Close()
 
-	var seq uint64
-	for _, idx := range indices {
-		content, err := sourceStorage.GetBlock(idx)
-		if err != nil {
-			return err
-		}
-
-		if err := g.flusher.AddTransaction(schema.OpSet, seq, content, idx, tlog.TimeNowTimestamp()); err != nil {
-			return err
-		}
-		seq++
+	type idxContent struct {
+		idx     int64
+		content []byte
 	}
+	var (
+		wg              sync.WaitGroup
+		numProcess      = runtime.NumCPU()
+		indicesCh       = make(chan int64, numProcess)
+		idxContentCh    = make(chan idxContent, numProcess)
+		errCh           = make(chan error)
+		doneCh          = make(chan struct{})
+		ctx, cancelFunc = context.WithCancel(context.Background())
+	)
+	defer cancelFunc()
+
+	// produces the indices we want to fetch
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, idx := range indices {
+			select {
+			case <-ctx.Done():
+				return
+			case indicesCh <- idx:
+			}
+		}
+		close(indicesCh)
+	}()
+
+	// fetch the indices
+	for i := 0; i < numProcess; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for idx := range indicesCh {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					content, err := sourceStorage.GetBlock(idx)
+					if err != nil {
+						errCh <- err
+						return
+					}
+					idxContentCh <- idxContent{
+						idx:     idx,
+						content: content,
+					}
+
+				}
+			}
+		}()
+	}
+
+	// add to flusher
+	var seq uint64
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for ic := range idxContentCh {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				err = g.flusher.AddTransaction(schema.OpSet, seq, ic.content, ic.idx, tlog.TimeNowTimestamp())
+				if err != nil {
+					errCh <- err
+					return
+				}
+				seq++
+				if int(seq) == len(indices) {
+					return
+				}
+			}
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		doneCh <- struct{}{}
+	}()
+
+	select {
+	case err := <-errCh:
+		return err
+	case <-doneCh:
+		// all is good
+	}
+
 	_, err = g.flusher.Flush()
-	log.Infof("GenerateFromStorage generates `%v` tlog data with err = %v", seq, err)
+	log.Infof("GenerateFromStorage generates `%v` tlog data with err = %v", len(indices), err)
 	return err
 }
 
