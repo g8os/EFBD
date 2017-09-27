g8os/EFBD

commit 3b1425b785ed564758417be5fbe0907c04b71afb

improve errors and docs for backup crypto key

nbd/ardb/backup/crypto.go
@@ -158,26 +158,46 @@ func (key *CryptoKey) Type() string {
 }
 
 func (key *CryptoKey) validate() error {
-	if key == nil {
-		return errors.New("nil crypto key")
-	}
-
 	return validateCryptoKey(key[:])
 }
 
 func validateCryptoKey(key []byte) error {
-	if key != nil && len(key) == CryptoKeySize {
-		for _, b := range key {
-			if b != 0 {
-				return nil
-			}
+	// ensure the key isn't nil
+	if key == nil {
+		return ErrNilCryptoKey
+	}
+
+	// ensure a key is given,
+	// and if it's given that it has the correct size
+	if len(key) != CryptoKeySize {
+		return ErrInvalidCryptoKeySize
+	}
+
+	// ensure that a key isn't all zeroes,
+	// as that would be the default for a static array key
+	for _, b := range key {
+		if b != 0 {
+			return nil
 		}
 	}
 
-	return errors.New("invalid crypto key")
+	// the key was all zeroes
+	return ErrInvalidCryptoKeyAllZeroes
 }
 
 func newKeyedHasher(ct CompressionType, ck CryptoKey) (zerodisk.Hasher, error) {
 	key := append(ck[:], byte(ct))
 	return zerodisk.NewKeyedHasher(key)
 }
+
+var (
+	// ErrInvalidCryptoKeySize is returned in case a
+	// given encryption key is not equal to `CryptoKeySize`.
+	ErrInvalidCryptoKeySize = errors.New("invalid crypto key size")
+	// ErrInvalidCryptoKeyAllZeroes is returned in case a
+	// given encryption key is all zeroes.
+	ErrInvalidCryptoKeyAllZeroes = errors.New("invalid all-zeroes crypto key")
+	// ErrNilCryptoKey is returned in case a
+	// given encryption key is nil.
+	ErrNilCryptoKey = errors.New("nil crypto key")
+)

zeroctl/cmd/backup/export.go
@@ -85,6 +85,9 @@ Remember to keep note of the used (snapshot) name,
 crypto (private) key and the compression type,
 as you will need the same information when importing the exported backup.
 
+The crypto (private) key has a required fixed length of 32 bytes,
+and cannot be all zeroes.
+
   If an error occured during the export process,
 deduped blocks might already have been written to the FTP server.
 These blocks won't be deleted in case of an error,

zeroctl/cmd/backup/import.go
@@ -167,6 +167,9 @@ Remember to use the same (snapshot) name,
 crypto (private) key and the compression type,
 as you used while exporting the backup in question.
 
+The crypto (private) key has a required fixed length of 32 bytes,
+and cannot be all zeroes.
+
   If an error occured during the import process,
 blocks might already have been written to the block storage.
 These blocks won't be deleted in case of an error,

commit 0400df37aa1dec24ab0d325a2d2c82be6ff7b1bd

fix broken master: crypto key nil dereference
this fixes the bug that got introduced in 3b1425b

nbd/ardb/backup/crypto.go
@@ -158,15 +158,14 @@ func (key *CryptoKey) Type() string {
 }
 
 func (key *CryptoKey) validate() error {
-	return validateCryptoKey(key[:])
-}
-
-func validateCryptoKey(key []byte) error {
-	// ensure the key isn't nil
 	if key == nil {
 		return ErrNilCryptoKey
 	}
 
+	return validateCryptoKey(key[:])
+}
+
+func validateCryptoKey(key []byte) error {
 	// ensure a key is given,
 	// and if it's given that it has the correct size
 	if len(key) != CryptoKeySize {