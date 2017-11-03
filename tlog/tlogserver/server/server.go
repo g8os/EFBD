824cf50946b752deac97f0794113a6fb787bfcae
Iwan Budi Kusnanto <ibk@labhijau.net>
2017-11-03 16:46:54 +0700

    tlog : reuse capnp encoder and decoder

--- tlog/tlogserver/server/server.go
+++ tlog/tlogserver/server/server.go
@@ -267,21 +267,6 @@ func (s *Server) readDecodeHandshakeRequest(r io.Reader) (*schema.HandshakeReque
 	return &resp, nil
 }
 
-// ReadDecodeClientMessage reads and decodes tlog message from client
-func (s *Server) ReadDecodeClientMessage(r io.Reader) (*schema.TlogClientMessage, error) {
-	msg, err := capnp.NewDecoder(r).Decode()
-	if err != nil {
-		return nil, err
-	}
-
-	cmd, err := schema.ReadRootTlogClientMessage(msg)
-	if err != nil {
-		return nil, err
-	}
-
-	return &cmd, nil
-}
-
 // listen for wait-tlog connection
 func (s *Server) listenTlogWait() {
 	log.Infof("s.coordListener listen at: %v", s.coordListener.Addr().String())
